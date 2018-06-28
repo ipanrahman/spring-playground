62051dc4f40685460131848e1644410c5433b8b2
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2018-06-28 14:53:24 +0700

    init admin template (Adminator)

---
+++ src/main/java/com/github/ngubex/spring/playground/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.github.ngubex.spring.playground.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Ipan Taupik Rahman
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
